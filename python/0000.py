ecb1665889ce1514cd2084859a4a3b8860ce6329
PEICX <peicx@outlook.com>
2017-03-11 16:37:01 +0800

    Complete 0010, improve 0000;

--- python/0000.py
+++ python/0000.py
@@ -1,10 +1,16 @@
 from PIL import Image, ImageDraw, ImageFont
 
-try:
-    im = Image.open('a.jpg')
+
+def add_num(im):
     draw = ImageDraw.Draw(im)
-    font = ImageFont.truetype("arial.ttf", 100)  # truetype 为全真字体，格式为ttf
-    draw.text((im.size[0]-120, 0), "99", fill=(255, 0, 0), font=font)  # fill参数可以用RGB颜色模式
-    im.save("test.jpg", "jpeg")  # 保存的格式为jpg，但是在参数里必须写jpeg
-except IOError:                 # 读取文件失败返回这个错误
-    print("读取失败")
+    # truetype 为全真字体，格式为ttf
+    font = ImageFont.truetype("arial.ttf", 100)
+    # fill参数可以用RGB颜色模式
+    draw.text((im.size[0]-120, 0), "99", fill=(255, 0, 0), font=font)
+    # 保存的格式为jpg，但是在参数里必须写jpeg
+    im.save("test.jpg", "jpeg")
+
+
+if __name__ == '__main__':
+    im = Image.open('a.jpg')
+    add_num(im)
